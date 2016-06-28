9c7f873a66b2ea0c474bb86284ae7aab450e430d
jcwyatt <jwyatt043@googlemail.com>
2016-06-28 21:11:52 +0000

    Used the functions example from Ben Nutall to establish the length of a flash on the meter as 0.05s.
    Code in this commit calculates the rate of consumption at every flash of the
    meter's LED, and converts it to a graphical representation (hoz. axis = consumption (arbitrary units).
    
    (Next mod will sample the consumtion rate every 10s.)

--- electracker.py
+++ electracker.py
@@ -1,22 +1,35 @@
 from gpiozero import Button
+from time import sleep
+from time import time
 
 ldr = Button(4)
 
-i = 0
 
+old_t = time()
+new_t = time()
 while True:
-	ldr.wait_for_press()
-	print("It got dark")
-	i +=1
-	print (i)
+	ldr.wait_for_release()
+	old_t = new_t
+	new_t = time()
+	interval = new_t - old_t
+	print (int(interval*10)*" " + "*")
+	sleep (0.07)
+	
 
-'''def hello():
-    print("Hello")
+'''while True:
+	def hello():
+    		print("Hello")
+		x = time()
+		print (x)
+		return x
 
-def bye():
-    print("Bye")
-
-button.when_pressed = hello
-button.when_released = bye
+	def bye():
+    		print("Bye")
+		y = time()
+		print (y)
+		return y
+	
+	ldr.when_pressed = hello = q
+	ldr.when_released = bye
+	print (q)
 '''
-
