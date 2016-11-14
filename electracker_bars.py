69f9a28f9ba96da3d9277765b348154c477d3638
jcwyatt <jwyatt043@googlemail.com>
2016-11-14 22:54:33 +0000

    Modified to write to thing speak and made a new version of the program, electracker _bars.py that records for an interval and then logs the consumption.

--- electracker_bars.py
+++ electracker_bars.py
@@ -5,21 +5,23 @@ from time import time
 
 ldr = Button(4)
 
+barTime = 60 #length of each record cycle
 
-old_t = time()
-new_t = time()
-logOfData=[]
-dataPoints=0
 while True:
-	ldr.wait_for_release()
-	old_t = new_t
-	new_t = time()
-	interval = new_t - old_t
-	consumption = 1.125/interval
-#	print (int(interval*10)*" " + "* " +str(consumption))
+	flashes = 0
+	startTime = time()
+	endTime = startTime + barTime
+	while time()<=endTime:
+	
+		ldr.wait_for_release()
+		flashes += 1
+		sleep(0.7)			
+	
+	consumption = 1.125/(barTime/flashes)
+#	print (int(flashes*5)*" " + "* " +str(consumption))
 
-#	print(time())
-#write to thingspeak
+#write to thingspeak:
+
 	params = urllib.urlencode({'field1': consumption, 'field2': 18,'key':'4WCP4S3W6MJAF4X5'})
 	headers = {"Content-type": "application/x-www-form-urlencoded","Accept":"text/plain"}
 	conn = httplib.HTTPConnection("api.thingspeak.com:80")
@@ -28,6 +30,4 @@ while True:
 	print response.status, response.reason
 	data = response.read()
 	conn.close()
-	
-	
 
