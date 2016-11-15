ccfc0e797fc3dc07b3623b2b1af292dc08378516
jcwyatt <jwyatt043@googlemail.com>
2016-11-15 22:12:45 +0000

    Amended program to fix error (sleep 0.07 was sleep 0.7). Amended write interval to 20s not 60. Added try/except to prevent hang on no connect to thingspeak.
    Also amended ping check in crontab to 5 minutes (was 20) and added this project to local.rc to auto run.
    
    Seems to be working.

--- electracker_bars.py
+++ electracker_bars.py
@@ -5,7 +5,7 @@ from time import time
 
 ldr = Button(4)
 
-barTime = 60 #length of each record cycle
+barTime = 20  #length of each record cycle
 
 while True:
 	flashes = 0
@@ -15,19 +15,22 @@ while True:
 	
 		ldr.wait_for_release()
 		flashes += 1
-		sleep(0.7)			
-	
+		#print("flash" + str(flashes))
+		sleep(0.07)			
+		
 	consumption = 1.125/(barTime/flashes)
 #	print (int(flashes*5)*" " + "* " +str(consumption))
 
 #write to thingspeak:
 
-	params = urllib.urlencode({'field1': consumption, 'field2': 18,'key':'4WCP4S3W6MJAF4X5'})
-	headers = {"Content-type": "application/x-www-form-urlencoded","Accept":"text/plain"}
-	conn = httplib.HTTPConnection("api.thingspeak.com:80")
-	conn.request("POST", "/update", params, headers)
-	response = conn.getresponse()
-	print response.status, response.reason
-	data = response.read()
-	conn.close()
-
+	try:
+		params = urllib.urlencode({'field1': consumption, 'field2': 18,'key':'4WCP4S3W6MJAF4X5'})
+		headers = {"Content-type": "application/x-www-form-urlencoded","Accept":"text/plain"}
+		conn = httplib.HTTPConnection("api.thingspeak.com:80")
+		conn.request("POST", "/update", params, headers)
+		response = conn.getresponse()
+		print response.status, response.reason
+		data = response.read()
+		conn.close()
+	except:
+		print("Couldn't write to TS")
